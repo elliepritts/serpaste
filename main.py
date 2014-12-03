33cf549874a20a601c24594b2d4f1520fcb2228e
Ellie <ellie@elliepritts.com>
2014-12-02 21:12:04 -0600

    removed not so magic numbers, include header of image

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from os import listdir
 import re
 from PIL import Image
 
+iOSHeader = 129 # this is the sizing of the header to be removed
+
 ignoredFiles = ['.DS_Store', 'composite.PNG']
 
 def smartnumbers(n):
@@ -16,18 +18,23 @@ def list_screenshots(directory):
     list.sort(key=smartnumbers)
     return list
 
-path = '/Users/ellie/Desktop/ASSets'
+path = '/Users/ellie/Desktop/images'
 myFileNames = list_screenshots(path)
 
-compositeImage = Image.new("RGB", (640, 10000), "white")
+firstImage = Image.open(path + '/' + myFileNames[0])
+(width, height) = firstImage.size
+
+compositeImage = Image.new("RGB", (width, len(myFileNames) * height), "red")
 
 n = 0
 
 for fileName in myFileNames:
     screenshotImage = Image.open(path + '/' + fileName)
-    compositeImage.paste(screenshotImage, (0, n * 1136))
+    if n > 0:
+        screenshotImage = screenshotImage.crop((0, iOSHeader, width, height))
+        compositeImage.paste(screenshotImage, (0, n * (height - iOSHeader)))
+    else:
+        compositeImage.paste(screenshotImage, (0, 0))
     n = n + 1
 
 compositeImage.save(path + '/' + "composite.PNG", "PNG")
-
-
